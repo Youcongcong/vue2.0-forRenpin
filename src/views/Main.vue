dd79eccdf3940019b7bac6cc8b4ea9dd6a7c7fb5
ycc11133 <834153504@qq.com>
2017-06-12 09:31:28 +0800

    first commit

---
+++ src/views/Main.vue
@@ -0,0 +1,18 @@
+<template>
+	<section>
+		main123
+		<div class="aa">
+			<div class="bb"></div>
+		</div>
+	</section>
+</template>
+
+<script>
+	export default {
+	}
+
+</script>
+
+<style scoped lang="scss">
+	@import '~scss_page';
+</style>
